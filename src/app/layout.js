19c905db9039943aef534b3ab245cfc38cad4c0f
wadud115 <wadud871226@gmail.com>
2026-09-12 22:14:06 +0600

    Naver banner and footer section added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit} from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navber";
+import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const OutfitFont = Outfit({
+  
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata = {
   title: "Create Next App",
@@ -21,13 +19,13 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${OutfitFont.className}  h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
            <Navbar></Navbar>
      
         {children}
-        
+        <Footer></Footer>
         </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
+import Banner from "@/components/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+
+      <Banner></Banner>
      
     </div>
   );
